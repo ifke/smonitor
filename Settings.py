108554bfc362342e8b5b63b48ce80257f59b5e64
Alexandr Larin <larin@lifenet.ru>
2013-11-26 18:05:11 +0400

    Define the example Settings.py file

--- Settings.py
+++ Settings.py
@@ -5,36 +5,21 @@
 
 switches = [
     {
-        'name': 'procurve2520.forb.local',
-        'ip': '172.17.5.221',
+        'name': 'switch1.example.local',
+        'ip': '192.168.0.254',
         'nports': 28,
+    },
+    {
+        'name': 'switch2.example.local',
+        'ip': '192.168.0.253',
+        'nports': 24,
     },
 ]
 zabbix_server = '127.0.0.1'
-community = 'private'
-#mac2ip_enable = 0
-#ip2fqdn_enable = 0
-#show_all_addresses = 0
+community = 'public'
+mac2ip_enable = 1
+ip2fqdn_enable = 1
 
-send2zabbix_interval = 60
+send2zabbix_interval = 90
 
-vendors_file = ''
-#oui_file = 'oui.txt'
-#addresses_file = '../tmp/smonitor.txt'
-#arpscan_only = 'eth0'
-
-#loglevel = 7
-
-#default_number_of_ports = 48
-#max_hosts_on_port = 7
-
-#ifconfig_cmd = '/sbin/ifconfig'
-#sudo_cmd = '/usr/bin/sudo'
-#nmap_cmd = '/usr/bin/nmap'
-#zabbix_sender_cmd = '/usr/bin/zabbix_sender'
-#snmpwalk_cmd = '/usr/bin/snmpwalk'
-
-#pidfile = '/var/run/smonitor/smonitor.pid'
-
-#mac_oid = '.1.3.6.1.2.1.17.4.3.1.1'
-#port_oid = '.1.3.6.1.2.1.17.4.3.1.2'
+loglevel = 5
